2ac51e270c19f63d0b6152204e3ba0e3a0a74cba
brooksreed <brooksr8@gmail.com>
2015-05-27 20:04:00 -0400

    starting addition of ACK history handling
    changes prototyped at end of JLSJumpEstimator

--- core/JLSJumpEstimator.m
+++ core/JLSJumpEstimator.m
@@ -38,3 +38,59 @@ if(isempty(ackInds))
 else
     KFstart = min((t-tm),min(ackTimes));
 end
+
+return
+
+%% future -- with ACK histories
+
+%pass tNoACK in/out, input nACKHistory
+%(think about how this might change with exp version, where don't have
+%access to full history of acks in the sim)
+
+tNoAck  = tNoAck+1; % always increment 1 step at beginning
+
+% limit "lookback" to the length of the ACK history sent
+for i = 1:Nv
+    if(tNoAck(i) > nACKHistory)
+        tNoAck(i) = nACKHistory;
+        disp('WARNING: number of dropped ACKs exceeds ACK history')
+    end
+end
+    
+if(~isempty(ackInds))
+    
+    % times where ACK will update Dh
+    % ACK RD'd: go back and update DHat for ackInds at ackTimes
+    % (Dh init to pi.*alphaBar -- only update times where ACK changes)
+    
+    for i = 1:length(ackInds)
+        ii = ackInds(i);
+        % note... ackTimes indexed same as ackInds
+        % tNoACK indexed over 1,...,Nv (so use ii)
+        for tprime = ackTimes(i)-tNoACK(ii):ackTimes(i)
+            alphat(ii,tprime-tc) = alpha(ii,...
+                tprime-tc);
+        end
+    end
+    
+    % separate Dh loop because do whole makeD for all i at once
+    for i = 1:length(ackInds)
+        for tprime = ackTimes(i)-tNoACK(ii):ackTimes(i)
+            Dh(:,:,tprime) = makeD(Pi(:,tprime-tc),alphat(:,tprime-tc),...
+                Nu,Np);
+        end
+    end
+    
+end
+
+if(isempty(ackInds))
+    KFstart = t - tm;   % usual 1-step update
+else
+    % if ACKs, KF start goes back to oldest useful (eg new) ACK
+    KFstart = min((t-tm),min(ackTimes-tNoAck(ackInds)));
+end
+
+for i = 1:length(ackInds)
+    tNoACK(ackInds(i)) = 0; % for ackInds, reset counter to zero    
+end
+
