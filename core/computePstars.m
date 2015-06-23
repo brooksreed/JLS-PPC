8e03921de19638ea5f03fb9ee3a75c8144b2d029
Brooks Reed <brooksr8@gmail.com>
2015-06-23 01:06:07 -0400

    cleanup and comments in computePstars

--- core/computePstars.m
+++ core/computePstars.m
@@ -1,68 +1,56 @@
 function E_termsSquared = computePstars(nStars)
 
 % BR 6/22/2015
-% (first add after initial local dev)
 
-% confirm that terms are similar
-% just run this for largest nStars (not loop)
-
+% G describes the rearranging of terms
+% u(t,t), u(t,t-1), etc. to (u(t,t)-u(t,t-1)) etc.  
 G = zeros(nStars+1,nStars);
 G(1,1:nStars) = ones(1,nStars);
 G(2:nStars+1,1:nStars) = diag(-ones(1,nStars));
 
+% Define some symbolic vars
+% ab = alphaBar, at = alpha(t)
 syms ab
 Eat = ab;
 syms at
 
+singleCoeffs = cell(nStars,1);
+% coeffs for u(t), u(t,t-1) before rearranging
 singleCoeffs{1} = [(at-ab);(1-at)-(1-ab)];
-
-for star = 1:nStars
-    % looping through whole thing (should be fast)
-    % but maybe unnecessary?  
-    if(star>1)
-        str = sprintf('syms atm%d',star-1);
-        eval(str)
-        dingus = ['(1-at)*'];
-        dangus = ['(1-at)*'];
-        for i = 1:(star-2)
-            dingus = [dingus sprintf('(1-atm%d)*',i)];
-            dangus = [dangus sprintf('(1-atm%d)*',i)];
-        end
-        dingus = [dingus sprintf('atm%d-(1-ab)^%d*ab',star-1,star-1)];
-        dangus = [dangus sprintf('(1-atm%d)-(1-ab)^%d',star-1,star)];
-
-        singleCoeffs{star} = sym('tmp',[star+1,1]);
-        singleCoeffs{star}(1:star-1,:) = singleCoeffs{star-1}(1:star-1,:);
-        singleCoeffs{star}(star,:) = dingus;
-        singleCoeffs{star}(star+1,:) = dangus;
+% loop through recursively creating the estimator error expressions
+for star = 2:nStars
+    
+    % new alpha(t-(star-1)) var
+    str = sprintf('syms atm%d',star-1);
+    eval(str)
+    
+    % construct new terms (2nd-to-last and last) 
+    dingus = ['(1-at)*'];
+    for i = 1:(star-2)
+        dingus = [dingus sprintf('(1-atm%d)*',i)];
     end
+    dangus = [dingus sprintf('atm%d-(1-ab)^%d*ab',star-1,star-1)];
+    dungus = [dingus sprintf('(1-atm%d)-(1-ab)^%d',star-1,star)];
+    
+    % construct coefficient vector
+    singleCoeffs{star} = sym('tmp',[star+1,1]);
+    singleCoeffs{star}(1:star-1,:) = singleCoeffs{star-1}(1:star-1,:);
+    singleCoeffs{star}(star,:) = dangus;
+    singleCoeffs{star}(star+1,:) = dungus;
+    
 end
 
-
-
-%{
-% FOR NOW - hardcoded in needed syms
-% and definitions of expected values
-% (will there be any individual E__ that don't equal ab?)
-% P**
-%syms ab at atm1 
-Eat = ab;
-Eatpow2 = ab;
-Eatm1 = ab;
-Eatm1pow2 = ab;
-% P***
-%syms atm2
-Eatm2 = ab;
-Eatm2pow2 = ab;
-%}
-
-
+% solve linear system to rearrange terms
 % (note -- atm* are unique as input here, so don't end up with ^power)
 diffCoeffs = G\singleCoeffs{nStars};
+term = cell(nStars,1);
 for i = 1:nStars
     term{i} = diffCoeffs(i);
 end
-    
+
+% square terms and store in string form
+termsSquared = cell(nStars,1);
+termStr = cell(nStars,1);
 for i = 1:nStars
     
     termsSquared{i} = expand(term{i}^2);    
@@ -72,7 +60,7 @@ for i = 1:nStars
     expr = '(?<!ab)\^2';
     tmp = regexprep(char(termsSquared{i}),expr,'pow2');
 
-    % check to make sure no higher powers
+    % check to make sure no higher powers - shouldn't be
     exprTest = '(?<!ab)\^\d';
     tmpTest = regexprep(tmp,exprTest,'TEST');
     if(~isempty(strfind(tmpTest,'TEST')))
@@ -87,12 +75,10 @@ for i = 1:nStars
     expr2 = '([\w]*at[\w]*)';
     termStr{i} = regexprep(tmp,expr2,'Eat');
     
-    % OLD:     
-    % add E to beginning of any term with "at"
-    %termStr{i} = regexprep(tmp,expr2,'E$0');
-    
 end
 
+% plug in and evaluate strings 
+E_termsSquared = cell(nStars,1);
 for i = 1:nStars
     E_termsSquared{i} = eval(termStr{i});
     fprintf('%d:   ',i);disp(factor(E_termsSquared{i}))
